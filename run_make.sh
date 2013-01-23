a81f951d5250c7bf2b737e254822d212b2fde107
devrenax <cm4is01_devrenax@yahoo.co.jp>
2013-01-23 13:46:21 +0900

    update build script

--- run_make.sh
+++ run_make.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-export CCOMPILER=~/Android/PONYO/CM/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
-#export CCOMPILER=~/Android/PONYO/CM/prebuilt/linux-x86/toolchain/gcc-linaro-4.7-2012.09/bin/arm-eabi-
+export CCOMPILER=~/Android/cyanogenmod/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
 export KERNEL_DIR=$PWD
 
 make ARCH=arm CROSS_COMPILE=$CCOMPILER -j`grep 'processor' /proc/cpuinfo | wc -l`
