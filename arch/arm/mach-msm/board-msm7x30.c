c3762480b1fb63e0c698d08215d3d3b957c13282
SumaPhone <128268582+SumaPhone@users.noreply.github.com>
2023-04-01 08:48:14 +0900

    Revert "fix kgsl_device_platform_data structure"
    This reverts commit ff78ef659752444e7b6b62245d84427c2b5ab774.

--- arch/arm/mach-msm/board-msm7x30.c
+++ arch/arm/mach-msm/board-msm7x30.c
@@ -5574,27 +5574,37 @@ static struct resource kgsl_3d0_resources[] = {
 };
 
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
-	.pwrlevel = {
-		{
-			.gpu_freq = 245760000,
-			.bus_freq = 192000000,
+	.pwr_data = {
+		.pwrlevel = {
+			{
+				.gpu_freq = 245760000,
+				.bus_freq = 192000000,
+			},
+			{
+				.gpu_freq = 192000000,
+				.bus_freq = 153000000,
+			},
+			{
+				.gpu_freq = 192000000,
+				.bus_freq = 0,
+			},
 		},
-		{
-			.gpu_freq = 192000000,
-			.bus_freq = 153000000,
-		},
-		{
-			.gpu_freq = 192000000,
-			.bus_freq = 0,
+		.init_level = 0,
+		.num_levels = 3,
+		.set_grp_async = set_grp3d_async,
+		.idle_timeout = HZ/20,
+		.nap_allowed = true,
+	},
+	.clk = {
+		.name = {
+			.clk = "grp_clk",
+			.pclk = "grp_pclk",
 		},
 	},
-	.init_level = 0,
-	.num_levels = 3,
-	.set_grp_async = set_grp3d_async,
-	.idle_timeout = HZ/20,
-	.nap_allowed = true,
-	.clk_map = KGSL_CLK_SRC | KGSL_CLK_CORE |
-		KGSL_CLK_IFACE | KGSL_CLK_MEM,
+	.imem_clk_name = {
+		.clk = "imem_clk",
+		.pclk = NULL,
+	},
 };
 
 static struct platform_device msm_kgsl_3d0 = {
@@ -5624,19 +5634,26 @@ static struct resource kgsl_2d0_resources[] = {
 };
 
 static struct kgsl_device_platform_data kgsl_2d0_pdata = {
-	.pwrlevel = {
-		{
-			.gpu_freq = 0,
-			.bus_freq = 192000000,
+	.pwr_data = {
+		.pwrlevel = {
+			{
+				.gpu_freq = 0,
+				.bus_freq = 192000000,
+			},
+		},
+		.init_level = 0,
+		.num_levels = 1,
+		/* HW workaround, run Z180 SYNC @ 192 MHZ */
+		.set_grp_async = NULL,
+		.idle_timeout = HZ/10,
+		.nap_allowed = true,
+	},
+	.clk = {
+		.name = {
+			.clk = "grp_2d_clk",
+			.pclk = "grp_2d_pclk",
 		},
 	},
-	.init_level = 0,
-	.num_levels = 1,
-	/* HW workaround, run Z180 SYNC @ 192 MHZ */
-	.set_grp_async = NULL,
-	.idle_timeout = HZ/10,
-	.nap_allowed = true,
-	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE,
 };
 
 static struct platform_device msm_kgsl_2d0 = {
